ba4e315de54620f84a7d947b82f416d5fc848218
jayverhoef <jay.verhoef@gmail.com>
2022-11-02 19:21:06 -0800

    1 Nov 2022 Chapter 11 Figures

--- Chapter11/Section 11.1/Section_11.1.R
+++ Chapter11/Section 11.1/Section_11.1.R
@@ -1,4 +1,4 @@
-sec_path = 'Rcode/Chapter9/Section 9.2/'
+sec_path = 'Rcode/Chapter11/Section 11.1/'
 setwd(paste0(SLEDbook_path,sec_path))
 
 #-------------------------------------------------------------------------------
@@ -10,28 +10,15 @@ setwd(paste0(SLEDbook_path,sec_path))
 #-------------------------------------------------------------------------------
 
 library("rgl")
-options(rgl.printRglwidget = FALSE)
-
-rgl.sphere <- function (x, y=NULL, z=NULL, ng=50, radius = 1, color="white", add=F, ...) {
-  lat <- matrix(seq(90, -90, len = ng)*pi/180, ng, ng, byrow = TRUE)
-  long <- matrix(seq(-180, 180, len = ng)*pi/180, ng, ng)
-
-  vertex  <- rgl:::rgl.vertex(x, y, z)
-  nvertex <- rgl:::rgl.nvertex(vertex)
-  radius  <- rbind(vertex, rgl:::rgl.attr(radius, nvertex))[4,]
-  color  <- rbind(vertex, rgl:::rgl.attr(color, nvertex))[4,]
-
-  for(i in 1:nvertex) {
-    add2 <- if(!add) i>1 else T
-    x <- vertex[1,i] + radius[i]*cos(lat)*cos(long)
-    y <- vertex[2,i] + radius[i]*cos(lat)*sin(long)
-    z <- vertex[3,i] + radius[i]*sin(lat)
-    persp3d(x, y, z, specular="white", add=add2, color=color[i], ...)
-  }
-}
-
+options(rgl.printRglwidget = TRUE)
 
 normalize <- function(v) v/sqrt(sum(v^2))
+
+normalize_hold3 = function(x) {
+	c(x[1]/sqrt((x[1] - x[3])^2 + (x[2]-x[3])^2), 
+		 x[2]/sqrt((x[1] - x[3])^2 + (x[2]-x[3])^2),
+		 x[3]) 
+}
 
 # These vectors all have the same length
 
@@ -48,46 +35,61 @@ y <- seq(from[2], to[2], length.out = 50)
 z <- seq(from[3], to[3], length.out = 50)
 #new 3d plot
 open3d()
-#add new sphere
-#spheres3d(center, radius = 1, col = "white", alpha = 0.1, 
-# fastTransparency = FALSE)
+
+
+rgl.clear( type = "shapes" )
 rgl.sphere(0, 0, 0, radius = 1, col = "white", alpha = 0, axes = FALSE,
 	xlab = '', ylab = '', zlab = '')
 
-#curve between two points
-arc3d(from, to, center, col = "red", lwd = 2)
+alpha = 10
+zlist = c(-rev(((1:30)/alpha)^2), 0, ((1:30)/alpha)^2)
+latcol = 'gray80'
+for(i in 1:length(zlist)) {
+	lat = c(1,1,zlist[i])
+	lat = normalize(lat)
 
-arc3d(top, to, center, col = 'blue')
-arc3d(to, down, center, col = 'blue')
-##arc3d(top, to_xy, center, col = 'blue')
-##arc3d(to_xy, down, center, col = 'blue')
-arc3d(top, from, center, col = 'blue')
-arc3d(from, down, center, col = 'blue')
-##arc3d(top, from_xy, center, col = 'blue')
-##arc3d(from_xy, down, center, col = 'blue')
+	arc3d(lat, c(-lat[1], lat[2], lat[3]), c(0,0,lat[3]), 
+		col = latcol)
+	arc3d(c(-lat[1], lat[2], lat[3]), c(-lat[1], -lat[2], lat[3]), 
+		c(0,0,lat[3]), col = latcol)
+	arc3d(c(-lat[1], -lat[2], lat[3]), c(lat[1], -lat[2], lat[3]), 
+		c(0,0,lat[3]), col = latcol)
+	arc3d(c(lat[1], -lat[2], lat[3]), c(lat[1], lat[2], lat[3]), 
+		c(0,0,lat[3]), col = latcol)
+}
+#theta = (1:32)/16
+#for(i in 1:length(theta)) {
+#	lon = c(sin(pi*theta[i]),cos(pi*theta[i]),0)
+#	lon = normalize(lon)
+#	arc3d(top, lon, center, col = latcol)
+#	arc3d(lon, down, center, col = latcol)
+#}
+latbands_col = 'green4'
+lwd_curves = 5
+for(i in 1:1) {
 
-#straight line
-rgl.lines(x,y,z, col = "black", lwd = 2)
+	arc3d(top, to, center, col = 'blue', lwd = lwd_curves)
+	arc3d(to, down, center, col = 'blue', lwd = lwd_curves)
+	arc3d(top, from, center, col = 'blue', lwd = lwd_curves)
+	arc3d(from, down, center, col = 'blue', lwd = lwd_curves)
 
-#lines3d(c(from[1], to[1]), c(from[2], to[2]),c(from[3], to[3]), col = "black")
-arc3d(to, c(-to[1], to[2], to[3]), c(0,0,to[3]), col = 'green')
-arc3d(c(-to[1], to[2], to[3]), c(-to[1], -to[2], to[3]), c(0,0,to[3]), col = 'green')
-arc3d(c(-to[1], -to[2], to[3]), c(to[1], -to[2], to[3]), c(0,0,to[3]), col = 'green')
-arc3d(c(to[1], -to[2], to[3]), c(to[1], to[2], to[3]), c(0,0,to[3]), col = 'green')
+	arc3d(to, c(-to[1], to[2], to[3]), 
+		c(0,0,to[3]), col = latbands_col, lwd = lwd_curves )
+	arc3d(c(to[1], -to[2], to[3]), c(to[1], to[2], to[3]), 
+		c(0,0,to[3]), col = latbands_col, lwd = lwd_curves)
+	arc3d(from, normalize_hold3(c(-to[1], to[2], from[3])), 
+		c(0,0,from[3]), col = latbands_col, lwd = lwd_curves, lty = 1)
+	arc3d(normalize_hold3(c(to[1], -to[2], from[3])), from, 
+		c(0,0,from[3]), col = latbands_col, lwd = lwd_curves)
 
-arc3d(from, c(-from[1], from[2], from[3]), c(0,0,from[3]), col = 'green')
-arc3d(c(-from[1], from[2], from[3]), c(-from[1], -from[2], from[3]), c(0,0,from[3]), col = 'green')
-arc3d(c(-from[1], -from[2], from[3]), c(from[1], -from[2], from[3]), c(0,0,from[3]), col = 'green')
-arc3d(c(from[1], -from[2], from[3]), c(from[1], from[2], from[3]), c(0,0,from[3]), col = 'green')
-
-# turn on html viewer widget
-options(rgl.printRglwidget = TRUE)
-
+	#curve between two points
+	arc3d(from, to, center, col = "red", lwd = lwd_curves)
+	#straight line
+	rgl.lines(x,y,z, col = "black", lwd = lwd_curves)
+}
 #add text
-text3d(rbind(from, to), 
+text3d(rbind(1.05*from, 1.2*to), 
        text = c(expression(s[1]), expression(s[2])),
-       depth_mask = FALSE, depth_test = "always", cex = 2)
-
-# print image from web browser and use krop to crop it
+       cex = 3, adj = 1)
 
 
